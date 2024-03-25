126868a2546c009867a9603c9055165a5f9d0925
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-03-25 19:28:57 +0330

    Set the app name.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    # Names and translations
+    verbose_name = _("Accounts")
