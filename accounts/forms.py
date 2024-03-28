df83d53133b0d1b6d33ab2d7628e7423cfb87f2f
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-03-28 11:13:22 +0330

    Create 'CustomUser' forms.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,28 @@
-from django import forms
+# from django import forms
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
 
 # Create your model forms here.
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "avatar",
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "bio",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
