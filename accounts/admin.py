ba895b6009e33d40254e0ea574a09c50250d19a4
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-03-28 11:36:26 +0330

    Create the admin site for 'CustomUser' model.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = (
+        "set_thumbnail_avatar",
+        "get_user_suggested_name",
+        "date_joined",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_editable = (
+        "is_active",
+    )
+    list_filter = (
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+    )
+    list_per_page = (
+        "35",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "username",
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "avatar",
+                    "bio",
+                ),
+            },
+        ), 
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "avatar",
+                    "bio",
+                ),
+            },
+        ),
+    )
