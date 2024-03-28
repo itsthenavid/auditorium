534b9c20f9f7dab542719dea0aa4c20f18b2c065
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-03-28 10:30:41 +0330

    Change comments.

--- accounts/apps.py
+++ accounts/apps.py
@@ -6,5 +6,5 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
-    # Names and translations
+    # Translation system
     verbose_name = _("Accounts")
