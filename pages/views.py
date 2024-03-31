3db067001726d576c7c86f41028a7167b51a585a
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-03-31 08:20:44 +0330

    Create the first instance of 'index' page.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_page_view(request):
+    template_name = None
+
+    match request.LANGUAGE_CODE:
+        case "en":
+            template_name = "index_en"
+        case _:
+            template_name = "index_fa"
+
+    return render(request, f"pages/{template_name}.html")
