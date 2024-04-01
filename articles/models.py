0b130938490e786996360d8fbc94c13046547599
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-04-01 17:59:30 +0330

    Write the base version of 'Article' model.

--- articles/models.py
+++ articles/models.py
@@ -59,3 +59,104 @@ class Category(models.Model):
     
     def __repr__(self) -> str:
         return self.name
+    
+
+class Article(models.Model):
+    STATUS_CHOICES = (
+        (str(0), _("Draft"), ), 
+        (str(1), _("Published"), )
+    )
+
+    artwork = models.ImageField(
+        _("Artwork"),
+        upload_to="articles/artworks/",
+        default="constants/default_artwork.webp"
+    )
+    title = models.CharField(
+        _("Title"),
+        max_length=125
+    )
+    description = models.CharField(
+        _("Description"),
+        max_length=355,
+        blank=True
+    )
+    datetime_created = models.DateTimeField(
+        _("Datetime Created"),
+        auto_now_add=True,
+    )
+    datetime_modified = models.DateTimeField(
+        _("Datetime Modified"),
+        auto_now=True,
+    )
+    slug = models.SlugField(
+        _("Slug"),
+        max_length=55
+    )
+    author = models.ForeignKey(
+        verbose_name=_("Author"),
+        to=_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="user_articles",
+        db_index=True
+    )
+    publish_datetime = models.DateTimeField(
+        _("Publish Datetime"),
+        default=now,
+        blank=True,
+        db_index=True
+    )
+    category = models.ForeignKey(
+        verbose_name=_("Article Category"),
+        to=Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="category_articles",
+        db_index=True
+    )
+    content = RichTextUploadingField(
+        _("Content"),
+        blank=True
+    )
+    status = models.CharField(
+        _("Status"),
+        choices=STATUS_CHOICES,
+        max_length=1,
+        default=STATUS_CHOICES[0][0],
+        db_index=True
+    )
+    is_active = models.BooleanField(
+        _("Active"),
+        default=True,
+        db_index=True
+    )
+
+    class Meta:
+        # Ordering system
+        ordering = (
+            "is_active",
+            "-publish_datetime"
+        )
+
+        # Translation system
+        verbose_name = _("Article")
+        verbose_name_plural = _("Articles")
+
+        # Database management system
+        indexes = (
+            models.Index(fields=[
+                "author",
+                "publish_datetime",
+                "category",
+                "status",
+                "is_active",
+            ], ), 
+        )
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def __repr__(self) -> str:
+        return self.title
