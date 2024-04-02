f85df6839524e2a060acd6c27736d49027acd775
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-04-02 17:49:52 +0330

    Create the first instance of Admin Panel.

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
+from .models import Category, Article
+
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "is_active",
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "set_artwork_thumbnail",
+        "title",
+        "publish_datetime",
+        "status",
+        "author",
+        "is_active",
+        "category",
+    )
+    list_editable = (
+        "status",
+        "category",
+    )
+    list_filter = (
+        "status",
+        "category",
+        "is_active",
+        "publish_datetime",
+    )
+    list_per_page = 15
+    search_fields = (
+        "title",
+        "description",
+        "category__title",
+    )
