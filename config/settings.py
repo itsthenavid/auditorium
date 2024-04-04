11ba1b5b24bd6f98907d9e6d895a9af019b6647d
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-04-04 19:45:24 +0330

    Change 'static' files direction.

--- config/settings.py
+++ config/settings.py
@@ -145,11 +145,11 @@ LOCALE_PATHS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static/"
+# STATIC_ROOT = BASE_DIR / "static/"
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static/",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 MEDIA_URL = "/media/"
 
