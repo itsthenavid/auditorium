8dd7ec3f26dedf31f1d8c1f6646ad358a45bbc83
Navid Rahimzadeh <itsthenavid@gmail.com>
2024-04-06 15:25:30 +0330

    Create the additional data for 'clusters' app.

--- clusters/apps.py
+++ clusters/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
-class ArticlesConfig(AppConfig):
+class ClustersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'articles'
+    name = 'clusters'
+
+    # Internationalization system
+    verbose_name = _("Clusters")
