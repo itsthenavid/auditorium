1e3f5a012d9a48039277c1929fb186e9beb8c7c9
Navid Rahimzadeh <itsthenavid@gmail.com>
2025-04-12 23:14:36 +0330

    Update project's default language to English.

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa-iran'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Tehran'
 
