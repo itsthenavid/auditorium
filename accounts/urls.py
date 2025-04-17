cba42dd83ab97ed448b6306e6cb25a5d069a7e88
Navid Rahimzadeh <itsthenavid@gmail.com>
2025-04-17 19:53:45 +0330

    Setup the base structure of 'accounts' system (Sign up, sign in & profile).

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from .views import ProfileView
+
 # Create your URL addresses here.
 
 urlpatterns = [
-    
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
-
