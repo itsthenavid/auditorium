e40fc10fed1302db13395bc8fd03ca0010b3c0c9
Navid Rahimzadeh <itsthenavid@gmail.com>
2025-04-19 06:29:44 +0330

    REMOVE ALL TEMPLATE SYSTEM.

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Hall, Post
 
 # Create your views here.
+
+
+class HallListView(ListView):
+    """
+    
+    """
+    
+    model = Hall
+    template_name = "documents/halls/list.html"
+
+
+class PostListView(ListView):
+    """
+    """
+
+    model = Post
+    template_name = "documents/posts/list.html"
