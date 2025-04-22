f399363be1e40f0c7d2d8004572168f7a8887716
Navid Rahimzadeh <itsthenavid@gmail.com>
2025-04-22 04:29:40 +0330

    Finish the 'Signup' page.

--- accounts/views.py
+++ accounts/views.py
@@ -23,4 +23,21 @@ class ProfileView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy("profile")
 
     def get_object(self):
-        return self.request.user 
+        return self.request.user
+
+    def form_valid(self, form):
+        avatar_file = self.request.FILES.get("avatar")
+        avatar_default = self.request.POST.get("avatar_default")
+        use_default = self.request.POST.get("use_default_avatar") == "true"
+
+        if use_default and avatar_default:
+            # Map the submitted path to the storage path
+            default_path = avatar_default.replace("/static/en/img/avatars/", "defaults/accounts/avatars/")
+            form.instance.avatar = default_path
+        elif avatar_file:
+            form.instance.avatar = avatar_file
+        else:
+            # Optionally handle case where no avatar is provided
+            pass
+
+        return super().form_valid(form)
