3aa3363250456686bab22ed5622ea0cf32f87c01
Navid Rahimzadeh <itsthenavid@gmail.com>
2025-06-25 06:12:23 +0330

    Add & install the 'Django Parler' package for management of multi-lang fields.

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third-party installed apps
     'django_celery_beat',
+    "parler",
     # Developer installed apps
     "accounts.apps.AccountsConfig",
     "extensions",
